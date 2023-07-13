f93ed8bc30e91794492860a99339a226d80428a1
jsimonassi <jsimonassi@id.uff.br>
2023-07-12 22:54:08 -0300

    Update branch and bound

--- branch-and-bound.pl
+++ branch-and-bound.pl
@@ -2,7 +2,10 @@
 room_size(4, 4).
 
 % Definir predicado para representar a posição da sujeira
+:- dynamic(goal_state/1).
 goal_state((1, 1)).
+goal_state((2, 3)).
+goal_state((3,3)).
 
 % Definir predicado para representar a posição do obstáculo
 obstacle(1, 2).
@@ -26,14 +29,9 @@ manhattan_distance((X1, Y1), (X2, Y2), Distance) :-
 
 % Definir predicado para calcular o custo do caminho atual
 calculate_path_cost([_], 0).
-calculate_path_cost([State1, State2 | Path], Cost) :-
-    manhattan_distance(State1, State2, SegmentCost),
-    calculate_path_cost([State2 | Path], RestCost),
-    Cost is RestCost + SegmentCost.
-
-% Definir predicado para buscar o caminho usando a busca Branch and Bound
-branch_and_bound(CurrentState, Path) :-
-    branch_and_bound([(CurrentState, [CurrentState], 0)], [], Path).
+calculate_path_cost([_, NextState | Path], Cost) :-
+    calculate_path_cost([NextState | Path], RestCost),
+    Cost is RestCost + 1.
 
 % Definir predicado para expandir um estado e obter seus sucessores
 expand_state((State, Path, _), Successors) :-
@@ -46,6 +44,10 @@ expand_state((State, Path, _), Successors) :-
         NextCost is PathCost + Distance
     ), Successors).
 
+% Definir predicado para buscar o caminho usando a busca Branch and Bound
+branch_and_bound(CurrentState, Path) :-
+    branch_and_bound([(CurrentState, [CurrentState], 0)], [], Path).
+
 % Definir predicado para buscar o melhor caminho usando a busca Branch and Bound
 branch_and_bound([], CurrentBestPath, CurrentBestPath).
 % Caso base: quando a lista de estados a serem explorados está vazia,
@@ -56,6 +58,7 @@ branch_and_bound([(CurrentState, CurrentPath, CurrentCost) | Rest], CurrentBestP
     calculate_path_cost(CurrentPath, PathCost),
     PathCost < CurrentCost,
     branch_and_bound([], CurrentPath, FinalBestPath).
+
 % Cláusula que é executada quando o estado atual é o estado objetivo.
 % Verifica se o custo do caminho atual é menor que o custo atualmente armazenado.
 % Se for, encontramos um caminho melhor e chamamos branch_and_bound/3 novamente
@@ -69,8 +72,24 @@ branch_and_bound([(CurrentState, CurrentPath, CurrentCost) | Rest], CurrentBestP
 % Em seguida, anexa os sucessores à lista Rest e à Expanded.
 % Chama branch_and_bound/3 novamente com a nova fila de estados (NewQueue).
 
+start_branch_and_bound(StartState, Path) :-
+    branch_and_bound(StartState, TempPath),
+    reverse(TempPath, Path),!.
+
+call_branch_and_bound([], []). 
+call_branch_and_bound([H | List], [Path|FinalPath]) :-
+	call_branch_and_bound(List, FinalPath),
+    start_branch_and_bound(H, Path).
+
+remove_last([_], []).
+remove_last([X|Xs], [X|Rest]) :-
+    remove_last(Xs, Rest).
+
+branch_and_bound_all(FinalPath) :-
+   	findall(Position, goal_state(Position), AllGoalPositions),
+    append([(0,0)], AllGoalPositions, AllStartPositions),
+    call_branch_and_bound(AllStartPositions, Path),
+    remove_last(Path, FinalPath).
 
 
-start_branch_and_bound(Path) :-
-    branch_and_bound((0, 0), TempPath),
-    reverse(TempPath, Path).
+
